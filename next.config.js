41b81f8baa7dc59af6274c2055b58985910e2141
AlbertIsernAlvarez <albert.isern.alvarez@gmail.com>
2024-04-23 16:42:00 +0200

    Fix

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ const nextConfig = {
           { key: "Access-Control-Allow-Credentials", value: "true" },
           {
             key: "Access-Control-Allow-Origin",
-            value: "*",
+            value: "https://mention.earth",
           },
           {
             key: "Access-Control-Allow-Methods",
