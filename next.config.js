bb9ce69e9a94b406edfd9aa0c2f503288f4d25eb
AlbertIsernAlvarez <albert.isern.alvarez@gmail.com>
2024-05-23 10:18:55 +0200

    chore: Update next.config.js to simplify Access-Control-Allow-Origin configuration

--- next.config.js
+++ next.config.js
@@ -1,25 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async headers() {
-    const allowedOrigins = ["https://mention.earth", "http://localhost:3000"];
-    const headers = allowedOrigins.map((origin) => ({
-      source: "/api/:path*",
-      headers: [
-        { key: "Access-Control-Allow-Credentials", value: "true" },
-        { key: "Access-Control-Allow-Origin", value: origin },
-        {
-          key: "Access-Control-Allow-Methods",
-          value: "GET,DELETE,PATCH,POST,PUT",
-        },
-        {
-          key: "Access-Control-Allow-Headers",
-          value:
-            "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-        },
-      ],
-    }));
-
-    return headers;
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
   },
 };
 
